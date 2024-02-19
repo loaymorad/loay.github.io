0237f64c077643f0fa4e912e4264fb897aabad57
loaymorad <loaymorad16@gmail.com>
2024-02-19 21:17:32 +0200

    add everything

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+if __name__ == '__main__':
+    app.run()
